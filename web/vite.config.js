a64d434413120b504de5de09ae5139d42fe5a3cb
angelogatz <angelogatzc@gmail.com>
2025-12-14 21:48:58 -0300

    feat: fullstack web message app and docker config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Necessário para o Docker mapear a porta
+    host: true,
     port: 5173, 
     watch: {
-      usePolling: true, // <--- A MÁGICA: Força o Vite a verificar mudanças
+      usePolling: true,
     },
   },
 })
